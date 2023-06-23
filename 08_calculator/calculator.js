83791c0eae90a8ec7a9fb8c6a72d72b1965c831c
Arsen <arsenpianist@gmail.com>
2023-06-23 14:13:31 -0700

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(...numbers) {
+  let result = 1;
 
+  numbers.forEach(num => {
+    result *= num;
+  });
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(inputNumber, power) {
+	let result = 1;
+
+  for (let i = 0; i < power; i++) {
+    result *= inputNumber;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(inputNumber) {
+	let result = inputNumber;
+
+  if (inputNumber === 0) {
+    return 1;
+  }
+  else {
+    for (let i = inputNumber-1; i > 0; i--) {
+      result *= i;
+    }
+  
+    return result;
+  }
 };
 
 // Do not edit below this line
