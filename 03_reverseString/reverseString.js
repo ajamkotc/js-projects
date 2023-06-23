1e6b0c46f7a773c0a29c47d0f15ae199b78ec828
Arsen <arsenpianist@gmail.com>
2023-06-23 10:40:46 -0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let input = inputString;
+    const charArray = input.split("");
+    let reverseString = "";
 
+    for (let i = charArray.length - 1; i >= 0; i--) {
+        reverseString += charArray[i];
+    }
+
+    return reverseString;
 };
 
 // Do not edit below this line
