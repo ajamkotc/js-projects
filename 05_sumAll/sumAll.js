2b3a30586e1eb6ab604dabe40efd0ade6332fb2e
Arsen <arsenpianist@gmail.com>
2023-06-23 11:33:42 -0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+    let i;
+    let greaterInt;
+    let returnInt = 0;
+    
+    if (typeof startInt !== "number" || typeof endInt !== "number") {
+        return "ERROR";
+    }
+    else if (startInt < 0 || endInt < 0) {
+        return "ERROR";
+    }
+    else if (startInt > endInt) {
+        i = endInt;
+        greaterInt = startInt;
+    }
+    else {
+        i = startInt;
+        greaterInt = endInt;
+    }
 
+    while (i <= greaterInt) {
+        returnInt += i;
+
+        i++;
+    }
+
+    return returnInt;
 };
 
 // Do not edit below this line
