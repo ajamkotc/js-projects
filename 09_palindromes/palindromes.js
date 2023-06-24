d5461ce587e90a39dfe307e6f7b5d949df08b9ff
Arsen <arsenpianist@gmail.com>
2023-06-24 11:21:05 -0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    let returnString = inputString.replace(/[.,!]/g, "");
+    returnString = returnString.replace(/\s/g, "");
+    returnString = returnString.toLowerCase();
 
+    let forward = 0;
+    let backward = returnString.length - 1;
+
+    while(forward < backward) {
+        if(returnString[forward] !== returnString[backward]) {
+            return false;
+        }
+
+        forward++;
+        backward--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
