4f5542aa14d68d73f736d5170d4363605b6726fd
Arsen <arsenpianist@gmail.com>
2023-06-27 13:33:35 -0700

    Complete etch-a-sketch project
    This commit inclues the finished etch-a-sketch game. The
    grid generating function was simplified by changing the
    grid display from flex to grid. The CSS and HTML files
    were changed accordingly.
    
    Functionality was added to the grid as well.

--- etch.js
+++ etch.js
@@ -1,24 +1,58 @@
 const mainContainer = document.querySelector("#container"); 
 
 //Create a 16x16 grid of divs
-function generateGrid() {
-    for(let u = 0; u < 16; u++) {
-        const newRow = document.createElement('div');
-        newRow.classList.add('row-container');
+function generateGrid(sideQuantity) {
+    mainContainer.setAttribute('style', `grid-template-columns: repeat(${sideQuantity}, 1fr)`);
+    if(sideQuantity > 100) {
 
-        for(let i = 0; i < 16; i++) {
+    }
+    else {    
+        for(let i = 0; i < sideQuantity*sideQuantity; i++) {
             const newCell = document.createElement('div');
             newCell.classList.add('grid-cell');
-            newRow.appendChild(newCell);
-        }
-        mainContainer.appendChild(newRow);
+            mainContainer.appendChild(newCell);
+        }    
+    }
+
+    //Changes the color of the tile on hover
+    const gridCell = document.querySelectorAll(".grid-cell");
+    gridCell.forEach((cell) => {
+        //Toggles color-change class to grid-cell which adds the color
+        cell.addEventListener('mouseenter', () => {
+            cell.classList.toggle('color-change');
+        });
+        /*
+        //Toggles color-change class to grid-cell which removes the color
+        cell.addEventListener('mouseout', () => {
+            cell.classList.toggle('color-change');
+        });
+        */
+    });
+}
+
+generateGrid(16);
+
+/* This function deletes the old grid by removing all children of the 
+ * mainContainer. 
+*/
+function clearGrid() {
+    while(mainContainer.firstElementChild) {
+        mainContainer.firstElementChild.remove();
     }
 }
 
-generateGrid();
 
-//Changes the color of the tile on hover
-const gridCell = document.querySelector(".grid-cell");
-gridCell.addEventListener('mouseover', () => {
-    alert("Hello, World!");
-});
+/* Prompts user for input when button is pressed and calls generateGrid
+ * with the inputted Number.
+*/
+function promptGrid() {
+    while(true) {
+        let newGridLength = prompt("Please enter the width of the new grid: ", 16);
+
+        if(newGridLength != null && !isNaN(newGridLength)) {
+            clearGrid();
+            generateGrid(newGridLength);
+            break;
+        }
+    }
+}
