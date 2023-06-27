4d381c74e4e19d03542f54147e45e5212195be4f
Arsen <arsenpianist@gmail.com>
2023-06-26 20:56:18 -0700

    Change generating function and add mouse listener.
    Grid width is now flexible, but not the height. The
    eventListener listening for the mouse does not work
    yet for grid cells.

--- etch.js
+++ etch.js
@@ -2,32 +2,23 @@ const mainContainer = document.querySelector("#container");
 
 //Create a 16x16 grid of divs
 function generateGrid() {
-    
-    //This loop creates 16 rows
-    for(let i = 0; i < 16; i++) {
+    for(let u = 0; u < 16; u++) {
         const newRow = document.createElement('div');
-        newRow.classList.add('grid-row-container');
+        newRow.classList.add('row-container');
 
-        //This loop populates each row with 16 divs
-        for(let u = 0; u < 16; u++) {
-            const newColumn = document.createElement('div');
-            newColumn.classList.add('grid-item');
-            //newColumn.textContent = 'this is a div';
-
-            newRow.appendChild(newColumn);
+        for(let i = 0; i < 16; i++) {
+            const newCell = document.createElement('div');
+            newCell.classList.add('grid-cell');
+            newRow.appendChild(newCell);
         }
         mainContainer.appendChild(newRow);
     }
-    
-    /*
-    for(let i = 0; i < quantity; i++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('grid-div');
-        newDiv.textContent = 'This is a div';
-        mainContainer.appendChild(newDiv);
-    }
-    */
 }
 
-//Call generateGrid and create a grid of 16 x 16
 generateGrid();
+
+//Changes the color of the tile on hover
+const gridCell = document.querySelector(".grid-cell");
+gridCell.addEventListener('mouseover', () => {
+    alert("Hello, World!");
+});
