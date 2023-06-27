0341bce3f5e78b87ff19a118054cbcf249d31376
Arsen <arsenpianist@gmail.com>
2023-06-27 13:50:52 -0700

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    const returnArray = [];
+    
+    for(let i = 0; i < bookArray.length; i++) {
+        let bookObj = bookArray[i];
+        returnArray.push(bookObj.title);
+    }
 
+    return returnArray;
 };
 
 // Do not edit below this line
